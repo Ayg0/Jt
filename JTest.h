48be5c582c6605c871454398cc86d09c871865dc
Ayg0 <taha.eddafili@gmail.com>
2024-07-25 17:05:31 +0100

    Color Stuff wa wa

--- JTest.h
+++ JTest.h
@@ -11,6 +11,11 @@ typedef struct testStruct{
 	int KOTests;
 } _testStruct;
 
+# define SUCC_COLOR "\033[1;37;42m"
+# define FAIL_COLOR "\033[1;37;41m"
+# define DEFF_COLOR "\033[0m"
+
+
 # define ASSERT_INT_EQU(EXPECTED, RESULT) isEqualInt(EXPECTED, RESULT, __LINE__)
 # define ASSERT_FLOAT_EQU(EXPECTED, RESULT) isEqualFloat(EXPECTED, RESULT, __LINE__)
 # define ASSERT_STR_EQU(EXPECTED, RESULT) isEqualStr(EXPECTED, RESULT, __LINE__)
--- jmain.c
+++ jmain.c
@@ -4,29 +4,29 @@ _testStruct tests;
 
 void isEqualInt(int expected, int resulted, int lineNb){
 	tests.TestsRan++;
-	fprintf(stderr, "Line: %d Test No %d: ", lineNb, tests.TestsRan);
+	fprintf(stderr, "Line: %03d Test No %03d: ", lineNb, tests.TestsRan);
 	if (expected == resulted)
-		fprintf(stderr, "PASSED\n"), tests.OKTests++;
+		fprintf(stderr, SUCC_COLOR "PASSED" DEFF_COLOR "\n"), tests.OKTests++;
 	else
-		fprintf(stderr, "FAILED Expected %d, resulted %d\n", expected, resulted), tests.KOTests++;
+		fprintf(stderr, FAIL_COLOR "FAILED" DEFF_COLOR" => Expected %d, resulted %d\n", expected, resulted), tests.KOTests++;
 }
 
 void isEqualFloat(float expected, float resulted, int lineNb){
 	tests.TestsRan++;
-	fprintf(stderr, "Line: %d Test No %d: ", lineNb, tests.TestsRan);
+	fprintf(stderr, "Line: %03d Test No %03d: ", lineNb, tests.TestsRan);
 	if (expected == resulted)
-		fprintf(stderr, "PASSED\n"), tests.OKTests++;
+		fprintf(stderr, SUCC_COLOR "PASSED" DEFF_COLOR "\n"), tests.OKTests++;
 	else
-		fprintf(stderr, "FAILED Expected %.3f, resulted %.3f\n", expected, resulted), tests.KOTests++;
+		fprintf(stderr, FAIL_COLOR "FAILED" DEFF_COLOR" => Expected %.3f, resulted %.3f\n", expected, resulted), tests.KOTests++;
 }
 
 void isEqualStr(const char *expected, const char *resulted, int lineNb){
 	tests.TestsRan++;
-	fprintf(stderr, "Line: %d Test No %d: ", lineNb, tests.TestsRan);
+	fprintf(stderr, "Line%03d Test No %03d: ", lineNb, tests.TestsRan);
 	if (!strcmp(expected, resulted))
-		fprintf(stderr, "PASSED\n"), tests.OKTests++;
+		fprintf(stderr, SUCC_COLOR "PASSED" DEFF_COLOR "\n"), tests.OKTests++;
 	else
-		fprintf(stderr, "FAILED Expected %s, resulted %s\n", expected, resulted), tests.KOTests++;
+		fprintf(stderr, FAIL_COLOR "FAILED" DEFF_COLOR" => Expected %s, resulted %s\n", expected, resulted), tests.KOTests++;
 }
 
 void jStart(){
